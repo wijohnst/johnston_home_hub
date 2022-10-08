0fdc222aa73c7bef51254c858aad94681ea48dd3
will-johnston-goodyear <will_johnston@goodyear.com>
2022-10-08 12:02:37 -0400

    Adds RecipeCollectionModal to MealPlanner

--- src/features/mealPlanner/MealPlanner.tsx
+++ src/features/mealPlanner/MealPlanner.tsx
@@ -7,41 +7,59 @@ import MealTable from "./MealTable";
 import { format } from "date-fns";
 import DayOfWeekButtonBar from "./DayOfWeekButtonBar";
 import { ListGroupContent } from "../../components/SharedComponents/ButtonBar";
+import RecipesCollectionModal from "../recipes/RecipesCollectionModal/RecipesCollectionModal";
 
 const MealPlanner = () => {
-	const {
-		data,
-		isFetching,
-		isFetched : areMealPlansFetched
-	} = useQuery("mealPlans", getMealPlans);
+  const {
+    data,
+    isFetching,
+    isFetched: areMealPlansFetched,
+  } = useQuery("mealPlans", getMealPlans);
 
-	const [selectedDayValue, setSelectedDayValue] = React.useState(0);
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const [_selectedRecipeId, setSelectedRecipeId] = React.useState<string | null>(null);
+  const [selectedDayValue, setSelectedDayValue] = React.useState(0);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_selectedRecipeId, setSelectedRecipeId] =
+    React.useState<string | null>(null);
+	const [isRecipesCollectionModalShown, setIsRecipesCollectionModalShown] = React.useState(false);
 
-	const getListGroupContentFromMealPlans = (mealPlans : MealPlans) : ListGroupContent => { 
-		return Object.keys(mealPlans).reduce<ListGroupContent | []>((listGroupArr, simplifiedDate) => { 
-			return [...listGroupArr, {
-				text: format(new Date(simplifiedDate), "EEE"),
-				value: parseInt(format(new Date(simplifiedDate), "i")) - 1,
-			}]
-		}, []);
-	}
+  /**
+   * Accepts a `MealPlans` object  and returns a `ListGroup` for rendering a `DayOfWeekButtonBar` with the inital selection being the current day 
+	 * 
+   * @param {MealPlans} mealPlans
+   * @returns { ListGroupContent}
+   */
+  const getListGroupContentFromMealPlans = (
+    mealPlans: MealPlans
+  ): ListGroupContent => {
+    return Object.keys(mealPlans).reduce<ListGroupContent | []>(
+      (listGroupArr, simplifiedDate, index) => {
+        return [
+          ...listGroupArr,
+          {
+            text: format(new Date(simplifiedDate), "EEE"),
+						value: index,
+          },
+        ];
+      },
+      []
+    );
+  };
 
   return (
     <MealPlannerWrapper>
       <h1>Meal Planner</h1>
-      {isFetching && !areMealPlansFetched && <span>Fetching Meal Plans...</span>}
+      {isFetching && !areMealPlansFetched && (
+        <span>Fetching Meal Plans...</span>
+      )}
       {areMealPlansFetched && data?.mealPlans && (
         <MealPlannerContent gap={3}>
           <DayOfWeekButtonBar
-						listGroupContent={getListGroupContentFromMealPlans(data.mealPlans)}
-						handleSelection={(value) => {
-							if (typeof value === 'number') { 
-								setSelectedDayValue(value)
-							}
-						}
-						}
+            listGroupContent={getListGroupContentFromMealPlans(data.mealPlans)}
+            handleSelection={(value) => {
+              if (typeof value === "number") {
+                setSelectedDayValue(value);
+              }
+            }}
           />
           {}
           <MealTable
@@ -51,6 +69,7 @@ const MealPlanner = () => {
           />
         </MealPlannerContent>
       )}
+			<RecipesCollectionModal isShown={isRecipesCollectionModalShown} handleHide={() => setIsRecipesCollectionModalShown(false) } />
     </MealPlannerWrapper>
   );
 };
